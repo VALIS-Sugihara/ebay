ad16d74e0bd209cdae51942e22053e01236d0237
sugihara.naoki <sugihara.naoki@revamp.co.jp>
2019-09-05 17:51:16 +0900

    Conf - ebay.yaml for SDK

--- functions/ebay.py
+++ functions/ebay.py
@@ -1,8 +1,27 @@
 import json
 import requests
+from ebaysdk.finding import Connection as finding
+# from ebaysdk import nodeText
 
 
 def test(event, context):
+    f = finding()
+    f.execute('findItemsAdvanced', {'keywords': 'shoes'})
+
+    dom = f.response_dom()
+    mydict = f.response_dict()
+    myobj = f.response_obj()
+
+    print(myobj.itemSearchURL)
+
+    return
+
+    # process the response via DOM
+    items = dom.getElementsByTagName('item')
+
+    for item in items:
+        print(nodeText(item.getElementsByTagName('title')[0]))
+
     # r = requests.get('https://google.com')
     # print(r.status_code)
     # TODO implement
@@ -10,3 +29,5 @@ def test(event, context):
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
+
+
